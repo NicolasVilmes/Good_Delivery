af88d97cbc2eab352464abd1055de01cb99986eb
Nicolas Vilmes <nicolasvilmes@outlook.com>
2023-02-01 18:14:52 -0500

    compleated signup page

---
+++ pages/[tenant]/signup.tsx
@@ -0,0 +1,124 @@
+import { Button } from "@/components/Button";
+import { Header } from "@/components/Header";
+import { InputField } from "@/components/InputField";
+import { useAppContext } from "@/contexts/AppContext";
+import { useApi } from "@/libs/useApi";
+import { Tenant } from "@/types/tenant";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styles from "../../styles/SignUp.module.css";
+
+const SignUp = (data: Props) => {
+  const { tenant, setTenant } = useAppContext();
+
+  useEffect(() => {
+    setTenant(data.tenant);
+  }, [data.tenant, setTenant]);
+
+  const router = useRouter();
+
+  const [name, setname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubimit = () => {};
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Cadastro | {data.tenant.name}</title>
+      </Head>
+
+      <Header
+        color={data.tenant.mainColor}
+        backHref={`/${data.tenant.slug}/login`}
+      />
+
+      <div className={styles.header}>{data.tenant.name}</div>
+
+      <div
+        className={styles.subtitle}
+        style={{ borderBottomColor: data.tenant.mainColor }}
+      >
+        Preencha os campos para cadastrar-se
+      </div>
+
+      <div className={styles.line}></div>
+
+      <div className={styles.formArea}>
+        <div className={styles.inputArea}>
+          <InputField
+            color={data.tenant.mainColor}
+            placeholder="Digite seu nome"
+            value={name}
+            onchange={setname}
+          />
+        </div>
+        <div className={styles.inputArea}>
+          <InputField
+            color={data.tenant.mainColor}
+            placeholder="Digite seu e-mail"
+            value={email}
+            onchange={setEmail}
+          />
+        </div>
+
+        <div className={styles.inputArea}>
+          <InputField
+            color={data.tenant.mainColor}
+            placeholder="Digite sua senha"
+            value={password}
+            onchange={setPassword}
+            password
+          />
+        </div>
+
+        <div className={styles.inputArea}>
+          <Button
+            color={data.tenant.mainColor}
+            label="Cadastrar-se"
+            onClick={handleSubimit}
+            fill
+          />
+        </div>
+
+        <div className={styles.loginArea}>
+          Já tem cadastro?
+          <Link legacyBehavior href={`/${data.tenant.slug}/login`}>
+            <a style={{ color: data.tenant.mainColor }}>Fazer Login</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default SignUp;
+
+type Props = {
+  tenant: Tenant;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { tenant: tenantSlug } = context.query;
+  const api = useApi();
+
+  //Get Tenant
+  const tenant = await api.getTenant(tenantSlug as string);
+  if (!tenant) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      tenant,
+    },
+  };
+};
